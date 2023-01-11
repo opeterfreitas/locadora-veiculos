8248388bbc1513d90392b73aba3e132c4543a743
Peter Freitas <opeterfreitas@gmail.com>
2023-01-11 10:46:36 -0300

    findAll para Cliente

--- src/main/java/com/locap/locadora/services/ClienteService.java
+++ src/main/java/com/locap/locadora/services/ClienteService.java
@@ -6,6 +6,7 @@ import com.locap.locadora.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class ClienteService {
     public Cliente findById(Integer id) {
         Optional<Cliente> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+    }
+
+    public List<Cliente> findAll() {
+        return repository.findAll();
     }
 }
